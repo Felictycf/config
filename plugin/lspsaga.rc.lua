fb57a231e05bfde7d55d6035a8c36e75bf64ab3a
键键爱涵涵 <felicity@FelicitydeMacBook-Pro.local>
2022-11-22 23:48:26 +0800

    first commit

---
+++ plugin/lspsaga.rc.lua
@@ -0,0 +1,36 @@
+local status, saga = pcall(require, "lspsaga")
+if (not status) then return end
+
+saga.init_lsp_saga {
+  server_filetype_map = {
+    typescript = 'typescript'
+  }
+}
+require 'lspconfig'.pyright.setup {}
+require 'lspconfig'.clangd.setup {}
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
+vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
+vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
+-- add hujian
+-- 查看 这个函数的引用情况
+vim.keymap.set('n', 'gx', vim.lsp.buf.references, opts)
+-- format
+vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
+-- code_action
+vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+-- workspace
+vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+vim.keymap.set('n', '<space>wl', function()
+  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, opts)
+--
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
